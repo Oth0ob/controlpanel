0bf716256f4f1a7f66af7653f8d9779f9b6cc474
Othoob <76228564+Oth0ob@users.noreply.github.com>
2024-07-03 08:31:12 +0300

    Add files via upload

---
+++ directions.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03 يوليو 2024 الساعة 07:27
+-- إصدار الخادم: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `directions`
+--
+
+CREATE TABLE `directions` (
+  `id` int(11) NOT NULL,
+  `direction` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `directions`
+--
+
+INSERT INTO `directions` (`id`, `direction`) VALUES
+(1, 'Right'),
+(2, 'Left'),
+(3, 'Forward'),
+(4, 'Back');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
